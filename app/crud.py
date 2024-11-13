4b7e66de3a01b7b2ce29ee3128d4f87247e38362
Rayner Kovachevich <raykovachevich22@gmail.com>
2024-11-13 17:48:30 +0100

    Adding User Authentication

--- app/crud.py
+++ app/crud.py
@@ -4,8 +4,24 @@ from . import models
 def get_movie(db: Session, movie_id: int):
     return db.query(models.Movie).filter(models.Movie.id == movie_id).first()
 
-def create_movie(db: Session, movie):
+def create_movie(db: Session, movie, user_id:int):
+    movie.owner_id = user_id
     db.add(movie)
     db.commit()
     db.refresh(movie)
     return movie
+
+def create_user(db: Session, username: str, password: str):
+    hashed_password = models.hash_password(password)
+    db_user = models.User(username=username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User). filter(models.User.username == username). first()
+
+def recommend_movies(db: Session, user_id: int):
+    liked_movies = db.query(models.Movie).filter(models.Movie.owner_id == user_id).all()
+    genres = [movie.genre for movie in liked_movies]
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, APIRouter, status
 from sqlalchemy.orm import Session
 from . import models, crud
 from .database import sessionLocal, engine, Base
+from .models import MovieCreate
 
 app = FastAPI()
 
@@ -14,10 +15,13 @@ def get_db():
     finally:
         db.close()
 
+       
+
 @app.post("/movies/")
-def create_movie(tittle: str, description: str, genre: str, rating: float, db: Session = Depends(get_db)):
+def create_movie(tittle: str, description: str, genre: str, rating: float, user_id: int, db: Session = Depends(get_db)):
+    user_id = 1
     movie = models.Movie(tittle=tittle, description=description, genre=genre, rating=rating)
-    return crud.create_movie(db=db, movie=movie)
+    return crud.create_movie(db=db, movie=movie, user_id=user_id)
 
 
 @app.get("/movies/{movie_id}")
@@ -26,3 +30,17 @@ def read_movie(movie_id: int, db: Session = Depends(get_db)):
     if movie is None:
         raise HTTPException(status_code=404, detail="Movie not found")
     return movie
+
+@app.post("/register")
+def register_user(username: str, password: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_username(db, username)
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username alredy taken")
+    return crud.create_user(db, username, password)
+
+@app.get("/movies/recommendations/{user_id}")
+def get_recommendations(user_id: int, db: Session = Depends(get_db)):
+    movies = crud.recommend_movies(db, user_id)
+    if not movies:
+        raise HTTPException(status_code=404, detail="No recommendations found")
+    return movies
